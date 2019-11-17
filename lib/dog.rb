a2c740d16b1ff76b3f064831c7fa6f18a192e8f1
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-17 20:42:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,7 +70,12 @@ class Dog
     dog
   end 
 end 
-  
+ def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+end   
 
 
 
